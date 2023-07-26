4103363bc03769185b09687bc8e45bccfe8784d5
Joanhui <joanhui.chiu@gmail.com>
2023-07-27 00:51:46 +0800

    v01

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // eslint-disable-next-line no-undef
-  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/':'/',
+  base: Nodejs.process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/':'/',
   plugins: [react()],
 })
